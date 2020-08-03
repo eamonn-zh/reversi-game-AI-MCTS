a052076c0c24e2390d426119ffe9e7838eb16ee6
Yiming Zhang <yza440@sfu.ca>
2020-08-03 01:45:17 -0700

    Initial commit

---
+++ FastApi_Server/main.py
@@ -0,0 +1,18 @@
+"""
+    CMPT 383 Final Project
+    FastApi Server
+    Author: Yiming Zhang
+"""
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def hello():
+    return "Hello World"
+
+
+if __name__ == '__main__':
+    uvicorn.run(app='main:app', host="0.0.0.0", port=8000, reload=True, debug=True)
